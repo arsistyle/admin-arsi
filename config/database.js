dfd9ff21619bce0af467e7e703c4834e1e8f71ff
israel larrondo <arsistyle@gmail.com>
2020-10-29 13:31:10 -0300

    connector

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
-      connector: "strapi-hook-mongoose",
+      connector: "mongoose",
       settings: {
         uri: env("DATABASE_URI"),
       },
