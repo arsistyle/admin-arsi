fb8c56f669f9965ee7095994b4eca2eace1d7938
israel larrondo <arsistyle@gmail.com>
2020-10-29 11:40:28 -0300

    Admin

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b9b345fdb4e66d50d38f0946fe668103'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ccb03598-a56e-48ba-9fe1-188224a7e7ea'
+};
